c12a0d29b54a5cdd5ac85e4d2d847eaa206c43e8
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2025-07-24 02:10:42 +0530

    backend issue solved

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-website-6qhx.onrender.com",
+  baseURL: "https://ecommerce-website-6qhx.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => await API.post("/user/signup", data);
