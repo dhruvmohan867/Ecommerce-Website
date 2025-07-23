92ed67093600b158d5bc964bfdea7def62261b9c
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2025-07-24 02:24:48 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use(cors());
+app.use(cors({
+  origin: "https://ecommerce-website-frontend3.onrender.com", // Adjust this to your client URL
+  credentials: true,
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
